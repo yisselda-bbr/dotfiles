35cd7fb40a2844882c8166b566605c53c87b1cc8
yisselda-bbr <yrhoc@bbrpartners.com>
2026-01-22 14:47:53 -0500

    Update install.sh to include Node and NVM installation steps.

--- install.sh
+++ install.sh
@@ -2,3 +2,15 @@
 ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
 ln -sf "$PWD/aliases.zsh" "$HOME/.aliases.zsh"
 ln -sf "$PWD/.gitconfig" "$HOME/.gitconfig"
+
+# Node & nvm setup
+export NVM_DIR="$HOME/.nvm"
+if [ ! -d "$NVM_DIR" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  source "$NVM_DIR/nvm.sh"
+fi
+if ! command -v node > /dev/null; then
+  nvm install --lts
+fi
