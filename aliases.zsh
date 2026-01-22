17ab4650d47224eb9bfdab9e96c02f32cac063a9
yisselda-bbr <yrhoc@bbrpartners.com>
2026-01-22 11:04:15 -0500

    Delete aliases.zsh

--- aliases.zsh
+++
@@ -1,6 +0,0 @@
-# Example alias, replace or expand from your real list
-alias gs='git status'
-alias gc='git commit'
-alias gp='git push'
-alias gl='git pull'
-# ... Add more here from your dev-setup repo ...
